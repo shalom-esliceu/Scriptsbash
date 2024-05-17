d40f9fb6e95415ad431cd1351466ebf06a0ba775
Tu Nombre <you@example.com>
2024-05-17 08:49:06 +0200

    Script visto en clase

---
+++ FredCalent.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#Genera un nombre aleatori entre 1 i 10:
+adivina=$(( ( RANDOM % 10 )  + 1 ))
+echo "el número a adivinar és $adivina"
+########################################################
+# Escriu el teu codi aqui
+########################################################
+INTENTS_TOTALS=3
+INTENTS_CONSUMITS=0
+while [[ INTENTS_CONSUMITS -lt INTENTS_TOTALS ]]
+do
+    INTENTS_CONSUMITS=$(( INTENTS_CONSUMITS + 1 ))
+    read -p "INTRODUEIX UN NÚMERO: " NUMERO_INTRODUIT
+
+    if [[ $NUMERO_INTRODUIT -lt adivina  ]]
+    then
+        echo "Fred!"
+    elif [[ $NUMERO_INTRODUIT -gt adivina  ]]
+    then
+        echo "Calent!"
+    else
+        echo "Eureka!"
+        exit 0
+    fi
+done
+
+########################################################
+# Fi del teu codi
+########################################################
+
+echo "El nombre que havies d'adivinar era: ${adivina}"
