f84fbc640aa9cdff7dfe5d828166f55634a16a88
Tu Nombre <you@example.com>
2024-05-17 10:12:57 +0200

    a

--- comptar_multiples.sh
+++ comptar_multiples.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 #COMPTAR QUANTS NÚMEROS PARELLS HI HA
 #ENTRE 1 I 100
+I=1
+FINAL=100
+QUANTS=0
+while [[ INICI -le FINAL ]]
+do
+    if [[ $((I % 2)) -eq 0 ]]
+    then
+        QUANTS=$((QUANTS+1))
+    fi
+    INICI
+done
