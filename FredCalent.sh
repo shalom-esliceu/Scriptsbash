26dd3842aa456de86f20ee6a5ec882d381430f84
Tu Nombre <you@example.com>
2024-05-31 09:19:18 +0200

    Completo

--- FredCalent.sh
+++ FredCalent.sh
@@ -2,26 +2,32 @@
 
 #Genera un nombre aleatori entre 1 i 10:
 adivina=$(( ( RANDOM % 10 )  + 1 ))
-echo "el número a adivinar és $adivina"
+echo "el número a advinar és $adivina"
 ########################################################
 # Escriu el teu codi aqui
 ########################################################
-
 INTENTS_TOTALS=3
 INTENTS_CONSUMITS=0
 while [[ INTENTS_CONSUMITS -lt INTENTS_TOTALS ]]
 do
-    INTENTS_CONSUMITS=$(( INTENTS_CONSUMITS + 1 ))
+    #consumim un intent
+    INTENTS_CONSUMITS=$((INTENTS_CONSUMITS+1))
+    #demanar a l'usuari que introdueixi un número
     read -p "INTRODUEIX UN NÚMERO: " NUMERO_INTRODUIT
 
-    if [[ $NUMERO_INTRODUIT -lt adivina  ]]
+    # Si el nombre introduït és més petit, l'script escriurà 
+    # per pantalla "Fred!"
+    if [[ $NUMERO_INTRODUIT -lt adivina ]]
     then
         echo "Fred!"
-    elif [[ $NUMERO_INTRODUIT -gt adivina  ]]
+    elif [[ $NUMERO_INTRODUIT -gt adivina ]]
     then
+        #Si el nombre introduït és més gran, l'script escriurà 
+        # per pantalla "Calent!"
         echo "Calent!"
     else
-        echo "Eureka!"
+        # Si el nombre introduït és igual, l'script escriurà per pantalla "Eureka!"
+        echo "eureka!"
         exit 0
     fi
 done
